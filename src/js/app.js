30639f3f1fd2c77d96be51d03187e79e3709bb06
Phyllis Yen <escapiststupor@users.noreply.github.com>
2021-03-14 20:23:47 +0800

    wip

--- src/js/app.js
+++ src/js/app.js
@@ -3,4 +3,9 @@ require('../css/main.css');
 
 // JavaScript
 window.$ = require('jquery');
+require('./jquery.min.js');
+require('./jquery.scrolly.min.js');
+require('./jquery.poptrox.min.js');
+require('./skel.min.js');
+require('./util.js');
 require('./main.js');
